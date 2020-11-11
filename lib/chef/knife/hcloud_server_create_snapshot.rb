68187f3888dd2421957412556755f1861a3fce52
Christian Eichhorn <c.eichhorn@webmasters.de>
2020-11-11 14:02:22 +0100

    * snapshot     * options added

--- lib/chef/knife/hcloud_server_create_snapshot.rb
+++ lib/chef/knife/hcloud_server_create_snapshot.rb
@@ -18,6 +18,16 @@ module KnifeHcloud
       :description => 'The Chef node name for your new server node',
       :proc => Proc.new { |o| Chef::Config[:knife][:chef_node_name] = o }
 
+    option :no_server_delete,
+      :long => '--no-server-delete',
+      :description => 'Server will not be deleted',
+      :proc => Proc.new { |o| Chef::Config[:knife][:no_server_delete] = true }
+
+    option :no_old_images_delete,
+      :long => '--no-server-delete',
+      :description => 'Server will not be deleted',
+      :proc => Proc.new { |o| Chef::Config[:knife][:no_old_images_delete] = true }
+
     def run
       server = hcloud_client.servers.detect do |server|
         server.name == server_name
@@ -44,6 +54,18 @@ module KnifeHcloud
         log_action action: action
 
         log "Image '#{description}' created (#{image.id})"
+
+        hcloud_client.images.select do |image|
+          image.type == 'snapshot' && image.description.start_with?(server_name) && image.description != description
+        end.each do |image|
+          log "Destroy old image '#{image.description}'"
+          image.destroy
+        end unless Chef::Config[:knife][:no_old_images_delete]
+        
+        unless Chef::Config[:knife][:no_server_delete]
+          action = server.destroy
+          log_action action: action
+        end
       else
         error "Server '#{server_name}' not found"
       end
